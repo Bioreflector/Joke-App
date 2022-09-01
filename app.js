f8f9938f0e5352d7d0c5d13fc6647fb910b91944
Bioreflector <Bioreflector@gmail.com>
2022-09-01 09:41:42 +0300

    start reconstruction

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
-const jokesColection =[]
+
 const jokesFavouriteColection = []
 
-function FormSerchJoike(form){
+function FormSerchJoike(form) {
     const getJokeBtn = form.querySelector('button')
-    const {random , categories , search , searchInput} = form
+    const { random, categories, search, searchInput } = form
+    let joke
 
     let url = 'https://api.chucknorris.io/jokes/random'
 
     random.checked = true
-    
-
-    function checkedRandom(){
-        if(random.checked){
+    function checkedRandom() {
+        if (random.checked) {
             searchInput.classList.add('hide-search-field')
             url = 'https://api.chucknorris.io/jokes/random'
         }
     }
-    
-    search.addEventListener('change' , () =>{
-        if(search.checked){
+    search.addEventListener('click', () => {
+        if (search.checked) {
             searchInput.classList.remove('hide-search-field')
+
         }
     })
-    
-    function getFetch(url){
+
+    function getFetch(url) {
         return fetch(url).then(data => data.json())
     }
-    async function getJoikes(){
-       jokesColection.push(await getFetch(url))
+    async function getJoikes() {
+        //    jokesColection.push(await getFetch(url))
+        joke = await getFetch(url)
     }
 
-    function rander(){
-            let item = jokesColection[jokesColection.length-1]
-            const jokeCard = document.createElement('div')
-            jokeCard.classList.add('joke-card')
-            const jokesText = document.createElement('p')
-            jokesText.innerText = `${item.value}`
-            jokeCard.insertAdjacentElement('beforeend' , jokesText)
-            form.insertAdjacentElement('afterend' , jokeCard)
-            console.log(jokesColection)
-        
+    function rander() {
+        const jokeCard = document.createElement('div')
+        jokeCard.classList.add('joke-card')
+        const jokesText = document.createElement('p')
+        jokesText.innerText = `${joke.value}`
+        jokeCard.insertAdjacentElement('beforeend', jokesText)
+        form.insertAdjacentElement('afterend', jokeCard)
+
     }
 
-    function showJoke(event){
+    async function showJoke(event) {
         event.preventDefault()
-        getJoikes()
-        setTimeout((rander), 1000)
+        await getJoikes()
+        rander()
+
+
     }
 
-    random.addEventListener('change' , checkedRandom)
-    getJokeBtn.addEventListener('click' , showJoke)
+    random.addEventListener('click', checkedRandom)
+    getJokeBtn.addEventListener('click', showJoke)
 }
 
 const formSearchJoke = FormSerchJoike(document.formSearchJoke)
-console.log(jokesColection)
 
 
